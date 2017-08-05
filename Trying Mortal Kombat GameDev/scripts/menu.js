91e1a2a59f893ccd5cc30fd691bd3d441f663ce9
FNAFmarine <30545266+FNAFmarine@users.noreply.github.com>
2017-08-05 14:26:19 +0400

    Add files via upload

---
+++ Trying Mortal Kombat GameDev/scripts/menu.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+
+var logo = $("<img/>").attr("src", "images/logo.png").appendTo("body");
+logo.css({
+"position":"absolute",
+"top":"50px",
+"left":"120px"
+});
+});
